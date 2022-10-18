fe6fdebe1e552e78166be73f9478485bd90115f2
manfengjun <2656863989@qq.com>
2022-10-18 20:35:55 +0800

    121

--- lg_work_admin/src/components/layouts/BaseHeader.vue
+++ lg_work_admin/src/components/layouts/BaseHeader.vue
@@ -18,15 +18,7 @@
     </el-menu-item>
     <el-sub-menu index="1">
       <template #title>设置</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
+      <el-menu-item index="2-1">注销</el-menu-item>
     </el-sub-menu>
   </el-menu>
 </template>
